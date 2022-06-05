dd92e0cbac5394c7cf7019e64247ccaaa302d78e
이종석 <iyouluv@naver.com>
2022-06-05 03:16:21 +0000

    MongoDB Connect

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express")
 const app = express()
-const PORT = 5000;
+const PORT = 3000;
+const mongoose = require("mongoose")
+
+mongoose.connect('mongodb+srv://jongseok:852456a@cluster0.oit5f.mongodb.net/?retryWrites=true&w=majority', {
+
+
+}).then(() => console.log('MongoDB 연결중...'))
+  .catch(err => console.log(err))
+
 
 app.get("/",(req,res)=>{
-    res.send("Hello World~~!")
+    res.send("Hello wwwWorld~~!")
 })
 
 app.listen(PORT,()=>console.log("wqeqwe"))
